78c010249db76db1000e557055b87ce497b4ef85
jtz-3 <itzogana@uwaterloo.ca>
2026-03-06 13:31:37 -0500

    Added support to retrieve  info for multiple nutrients

--- domain/nutrition.py
+++ domain/nutrition.py
@@ -9,20 +9,26 @@ class NutritionInfo:
         self.nutrient_cols = {'protein': 'ProteinPerGram',
                               'fat': 'FatPerGram',
                               'calories': 'CaloriesPerGram',
-                              'fiber': 'FiberPerGram'}
+                              'fiber': 'FiberPerGram',
+                              'carbs': 'CarbsPerGram'}
 
     # Yes I know calories are not nutrients
+    # Indicate a list of nutrients; returns a tuple of 
+    # quantities (g or cal) in the corresponding order.
     def get_info(self, nutrients):
-        query = "SELECT SUM(ri.QuantityInGrams * i." + self.nutrient_cols[nutrient] + ") \
-                FROM RecipeIngredient ri \
+        # Turn quantity list into a series of aggregate queries
+        agg = ['SUM(ri.QuantityInGrams * i.' + self.nutrient_cols[n] + ')' for n in nutrients]
+        agg = ','.join(agg)
+
+        query = 'SELECT ' + agg + 'FROM RecipeIngredient ri \
                 JOIN Ingredient i ON ri.IngredientId = i.Id \
-                WHERE ri.RecipeId = ?;"
+                WHERE ri.RecipeId = ?;'
         
         # Should we be checking here if the DB has been made?
         with sqlite3.connect('../db/recipes.db') as conn:
             cur = conn.cursor()
             cur.execute(query, (self.recipeId))
-            row = cur.fetchone()            # Later change to .fetchall if support for multiple nutrients added
+            rows = cur.fetchall()
             cur.close()
 
-        return row
+        return rows[0]
