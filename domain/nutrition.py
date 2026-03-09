749a4ef7f8aa38df162fb826a1db0be95d1dee61
jtz-3 <itzogana@uwaterloo.ca>
2026-03-09 13:53:02 -0400

    Changed NutritionInfo objects to store cached base servings, macro values

--- domain/nutrition.py
+++ domain/nutrition.py
@@ -1,34 +1,45 @@
 # Calculations to obtain nutritional info of a recipe
 # per base serving.
-
 import sqlite3
 
 class NutritionInfo:
+    nutrient_cols = {'calories': 'CaloriesPerGram',
+                    'protein': 'ProteinPerGram',
+                    'carbs': 'CarbsPerGram',
+                    'fat': 'FatPerGram',
+                    'fiber': 'FiberPerGram',
+                    }
+
     def __init__(self, recipeId):
         self.recipeId = str(recipeId)
-        self.nutrient_cols = {'protein': 'ProteinPerGram',
-                              'fat': 'FatPerGram',
-                              'calories': 'CaloriesPerGram',
-                              'fiber': 'FiberPerGram',
-                              'carbs': 'CarbsPerGram'}
+        self.base_servings = None
 
-    # Yes I know calories are not nutrients
-    # Indicate a list of nutrients; returns a tuple of 
-    # quantities (g or cal) in the corresponding order.
-    def get_info(self, nutrients):
-        # Turn quantity list into a series of aggregate queries
-        agg = ['SUM(ri.QuantityInGrams * i.' + self.nutrient_cols[n] + ')' for n in nutrients]
+        # Initialize all nutrition info and set class values. 
+        agg = ['SUM(ri.QuantityInGrams * i.' + col + ')' for col in NutritionInfo.nutrient_cols.values()]
         agg = ','.join(agg)
+        print(agg)
 
-        query = 'SELECT ' + agg + 'FROM RecipeIngredient ri \
+        nutrient_query = 'SELECT ' + agg + 'FROM RecipeIngredient ri \
                 JOIN Ingredient i ON ri.IngredientId = i.Id \
                 WHERE ri.RecipeId = ?;'
         
         # Should we be checking here if the DB has been made?
         with sqlite3.connect('../db/recipes.db') as conn:
             cur = conn.cursor()
-            cur.execute(query, (self.recipeId))
-            rows = cur.fetchall()
+            cur.execute(nutrient_query, (self.recipeId))
+            self.calories, self.protein, self.carbs, self.fat, self.fiber = cur.fetchall()[0]
+
+            cur.execute('SELECT BaseServings FROM Recipe WHERE Id = ?', (self.recipeId))
+            self.base_servings = cur.fetchone()[0]
             cur.close()
 
-        return rows[0]
+        # print('BASE SERVINGS:', self.base_servings)
+        # print('MACROS:', self.calories, self.protein, self.carbs, self.fat, self.fiber)
+
+    def get_info(self, nutrients):
+        return
+    
+    # Return a new NutritionInfo object scaled to the desired number of servings.
+    def scale(self, desired_servings):
+        scaled_meal = NutritionInfo(self.recipeId)
+        return scaled_meal
