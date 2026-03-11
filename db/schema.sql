e9f536d358bec82f2831454653a38d8682adea6e
jtz-3 <itzogana@uwaterloo.ca>
2026-03-11 13:27:54 -0400

    Updated schema for unit and unit conversion considerations

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+-- Protein/Carbs/Fat/Fiber measured in grams.
 CREATE TABLE Ingredient (
     Id INTEGER PRIMARY KEY,
     Name TEXT NOT NULL UNIQUE,
@@ -5,7 +6,13 @@ CREATE TABLE Ingredient (
     ProteinPerGram REAL NOT NULL,
     CarbsPerGram REAL NOT NULL,
     FatPerGram REAL NOT NULL,
-    FiberPerGram REAL NOT NULL
+    FiberPerGram REAL NOT NULL,
+
+    BaseUnit TEXT NOT NULL CHECK(
+        BaseUnit IN ('g','kg','oz','lb','floz','mL','L','cup','tsp','tbsp')),
+
+    -- Grams per BaseUnit, to be used to later populate QuantityInGrams (RecipeIngredient)
+    GramConversion REAL NOT NULL                
 );
 
 -- Recipe steps will be delimited with a special character
@@ -15,12 +22,22 @@ CREATE TABLE Recipe (
     Name TEXT NOT NULL UNIQUE,
     Steps TEXT NOT NULL,
     BaseServings INTEGER NOT NULL,
+    Description TEXT NOT NULL,
     Notes TEXT
 );
 
+/*
+- Unit stores original recipe unit to preserve readability
+- Quantity refers to original 'base' Unit
+- QuantityInGrams converts (with an appropriate ratio) the base unit to grams to facilitate nutrition calculations
+*/
 CREATE TABLE RecipeIngredient (
     RecipeID INTEGER NOT NULL,
     IngredientId INTEGER NOT NULL,
+
+    Quantity REAL NOT NULL,
+    Unit TEXT NOT NULL,
+
     QuantityInGrams REAL NOT NULL,
 
     PRIMARY KEY (RecipeId, IngredientId),
